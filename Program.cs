3906407c8bf325b13988f0b56b5bcd6e38b4e830
SzymonKolanowski <75213000+SzymonKolanowski@users.noreply.github.com>
2021-05-24 08:37:45 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Analiza_Collatza
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Podaj liczbę");
+			string tekst = Console.ReadLine();
+			int liczba = int.Parse(tekst);
+			
+
+			while (liczba != 1 )
+			{
+				if (liczba % 2 == 0)
+				{
+					 liczba = liczba / 2;
+					Console.WriteLine(liczba);
+					while (liczba != 1)
+					{
+						if (liczba % 2 == 0)
+						{
+							liczba = liczba / 2;
+							Console.WriteLine(liczba);
+							break;
+						}
+						else
+						{
+							liczba = (liczba * 3) + 1;
+							Console.WriteLine(liczba);
+							break;
+						}
+					}
+				}
+				else
+				{
+					 liczba = (liczba * 3) + 1;
+					Console.WriteLine(liczba);
+					while (liczba != 1)
+					{
+						if (liczba % 2 == 0)
+						{
+							liczba = liczba / 2;
+							Console.WriteLine(liczba);
+							break;
+						}
+						else
+						{
+							liczba = (liczba * 3) + 1;
+							Console.WriteLine(liczba);
+							break;
+						}
+					}					
+				}
+				
+			}
+
+			Console.WriteLine("koniec");
+		}
+	}
+}
